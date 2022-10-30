c76c0fad479893620312bc7a0cfd641ec3993247
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2022-10-29 22:42:23 -0400

    Working on javaScript practice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfRepeats) {
+    parseInt(numberOfRepeats);
+    let newString = 0
+    if (numberOfRepeats < 0) {return 'ERROR'}
+    else {
+for (let i = 0; i < numberOfRepeats; i++) {
+    newString++;
+}
+return string.repeat(newString);
 };
-
+}
 // Do not edit below this line
 module.exports = repeatString;
